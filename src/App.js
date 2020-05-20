425dbe13801909ff338df5b77e4db3f598640296
Christos <constantinides.ch@gmail.com>
2020-05-20 16:41:29 +0200

    Testing receiving endpoint data

--- src/App.js
+++ src/App.js
@@ -1,14 +1,30 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
-import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
   //setting state
   const [degreeList, setDegreeList] = useState([]);
 
+  const proxyUrl = "https://cors-anywhere.herokuapp.com/";
   const stanfordUrl = "https://registree-coding-challenge.glitch.me/stanford";
   const cornellUrl = "https://registree-coding-challenge.glitch.me/cornell";
+
+  let responseData = [];
+
+  fetch(proxyUrl + stanfordUrl)
+    .then((r) => {
+      const contentType = r.headers["Content-Type"];
+      if (contentType === "application/json") {
+        return r.json();
+      } else {
+        return r.text();
+      }
+    })
+    .then((data) => (responseData = data));
+  // .then(() => console.log("responseData", responseData));
+
+  console.log("response data:", responseData);
 
   return (
     <div className="App">
