a167963cc9cac46dbb6d4a7b693445279338a799
Christos <constantinides.ch@gmail.com>
2020-05-20 16:28:39 +0200

    Initial setup

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
+  //setting state
+  const [degreeList, setDegreeList] = useState([]);
+
+  const stanfordUrl = "https://registree-coding-challenge.glitch.me/stanford";
+  const cornellUrl = "https://registree-coding-challenge.glitch.me/cornell";
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Degree Data</h1>
     </div>
   );
 }
